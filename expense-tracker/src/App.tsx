3bd3b0d3146bf614222691f0cf4cb805ad7772cf
jyothinagaraja26 <jyothi.n2k9@gmail.com>
2022-04-12 16:10:06 +0530

    first commit

---
+++ expense-tracker/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ShowData from "./components/ShowData";
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+      <Routes>
+        <Route path="/" element={<ShowData/>}></Route>
+      </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
